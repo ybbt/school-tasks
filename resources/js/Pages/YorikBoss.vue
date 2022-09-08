7338bfc397dd4acc65ed57a5c904ac08704442cc
developer <olexiy.k@i.ua>
2022-09-08 20:06:16 +0300

    first commit

---
+++ resources/js/Pages/YorikBoss.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="h-screen w-full flex justify-center items-center bg-amber-50">
+  <h1 class="text-8xl text-red-700">Yorik Boss</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "YorikBoss"
+}
+</script>
+
+<style scoped>
+
+</style>
