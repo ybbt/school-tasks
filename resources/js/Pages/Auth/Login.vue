17c603a13c3e81435fcd923558e6abc88b01b783
ybbt <olexiy.k@>
2023-07-28 16:19:45 +0200

    Adding linc for registrate in Login-form

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -57,7 +57,7 @@ const submit = () => {
 <!--                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
                     Forgot your password?
                 </Link> -->
-
+                <Link :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Not registered yet?</Link>
                 <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
