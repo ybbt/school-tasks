849e3a36e86708ae94e29cc6c2e98337ff94aecf
ybbt <olexiy.k@>
2023-07-28 17:49:36 +0200

    smoll adding links to dashboard

--- resources/js/Layouts/TasksLayout.vue
+++ resources/js/Layouts/TasksLayout.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="fixed top-0 right-0 px-6 py-4 sm:block">
+    <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</Link>
+  </div>
   <h1 class="text-green-900 text-4xl text-center">Приклади</h1>
   <div class="flex justify-center ">
     <div class="py-5">
